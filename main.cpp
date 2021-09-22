d1e7e9bf50f9fabc99ee9797a7b854cbb039ff04
r3corp <baudecoisas@gmail.com>
2021-09-22 18:15:20 -0300

    adding

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 
-int main(){
+int main() {
 
-  std::cout << "Hello World!!" ; 
-  std::cin::get();
+    //Const concepts
+    int inConstantVariable = 3;
+    inConstantVariable=6; //perfectly fine
+    const int constantVariable = 6;
+    // constantVariable=4; //error as the content of cont shouldn't be changed
+    const int* constantPointer = new int;
+    //int * const pointerConstant;
 
-  return 0;
+    //*constantPointer = 3; // the value of the pointer content can't be changed
+    constantPointer = &constantVariable; // but the pointer itself can be redirected
+
+
+
+    //tenary operations
+    int ternaryInt = 0;
+    ternaryInt > 3 ? printf("Print if true") : printf("Print if false");
+
+    //equal to
+    if (ternaryInt > 3)
+        printf("Print if true");
+    else
+        printf("Print if false");
+
+
+    return 0;
 }
